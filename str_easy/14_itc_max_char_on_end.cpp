47fcd6be9102fbce8821431e97a5f0328649e1d4
smyilik <85386771+smyilik@users.noreply.github.com>
2021-11-11 09:05:51 +0300

    Update 14_itc_max_char_on_end.cpp

--- str_easy/14_itc_max_char_on_end.cpp
+++ str_easy/14_itc_max_char_on_end.cpp
@@ -2,7 +2,8 @@
 
 int itc_max_char_on_end(string str)
 {
-	long long len = itc_len(str), max = 0, kol = 0;
+	long long len = itc_len(str), max = 0;
+	int kol = 0;
 	for (int i = 0; i < len; i++) {
 		if ((str[i] >= '0') && (str[i] <= '9')) {
 			kol++;
